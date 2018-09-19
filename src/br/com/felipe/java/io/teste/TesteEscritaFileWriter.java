bfb322e6ae1000cededc8dd4e2ea3f5d3f0decea
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2018-09-18 22:45:26 -0300

    add test currentTimeMillis

---
+++ src/br/com/felipe/java/io/teste/TesteEscritaFileWriter.java
@@ -0,0 +1,25 @@
+package br.com.felipe.java.io.teste;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TesteEscritaFileWriter {
+    public static void main(String[] args) throws IOException {
+
+        long ini = System.currentTimeMillis();
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter("teste.txt"));
+
+        bw.write("541641564165 Teste Maluco agora");
+        bw.newLine();
+        bw.write("Esse é o fim do arquivo");
+
+        bw.close();
+
+        long fim = System.currentTimeMillis();
+
+        System.out.println("Passaram " + (fim - ini) + " milissegundos");
+
+    }
+}
