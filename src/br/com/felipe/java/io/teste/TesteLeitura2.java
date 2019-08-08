c772db0e2f1c849030923116a03c6896fd782a02
Felipe Rodrigues <felipe_rodriguesx@hotmail.com>
2019-08-08 17:42:23 -0300

    testando a classe scanner

---
+++ src/br/com/felipe/java/io/teste/TesteLeitura2.java
@@ -0,0 +1,20 @@
+package br.com.felipe.java.io.teste;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TesteLeitura2 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("contas.csv"));
+
+
+        while(scanner.hasNextLine()) {
+            String linha = scanner.nextLine();
+            System.out.println(linha);
+        };
+
+
+        scanner.close();
+    }
+}
